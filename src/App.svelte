aa00bb068530ab9531c98ceb86285d7458641ae7
warnerej <warnerej@mail.uc.edu>
2025-09-20 12:11:57 -0400

    init

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  import { onDestroy } from 'svelte';
+
+  let time = new Date().toLocaleTimeString();
+
+  const interval = setInterval(() => {
+    time = new Date().toLocaleTimeString();
+  }, 1000);
+
+  onDestroy(() => clearInterval(interval));
+</script>
+
+<main>
+
+  <div class="topnav">
+    <h1 id="title">Language Tracker</h1>
+    <p id="time">Hello Elliot, it's currently {time}</p>
+  </div>
+
+
+
+  <div class="card">
+    <Counter />
+  </div>
+
+</main>
+
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,23 @@
+<script>
+  let count = $state(0)
+  const increment = () => {
+    if (count < 24){
+      count += 1
+    }
+  }
+  const decrement = () => {
+    if (count > 0){
+      count -= 1
+    }
+  }
+</script>
+
+<button onclick={increment}>
+  +
+</button>
+<div id="count">
+  Hours Studied: {count}
+</div>
+<button onclick={decrement}>
+  -
+</button>
